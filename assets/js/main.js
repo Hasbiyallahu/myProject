7fbaae34c2ae070d59fba3bde403deafefd1b304
Olanrewaju Ibrahim <165801079+Hasbiyallahu@users.noreply.github.com>
2024-08-18 23:44:24 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,15 +13,3 @@ cancelIcon.addEventListener('click', function () {
   headerLinks.style.right = '-100vh';
   headerLinks.style.display = 'none';
 });
-
-// theme color change script
-let themeInput = document.getElementById('theme');
-const root = document.documentElement; // Get the root element (<html>)
-const primaryThemeColor = getComputedStyle(root).getPropertyValue('--primary-theme-color');
-themeInput.value = primaryThemeColor;
-
-themeInput.addEventListener('change', function () {
-  // Get the value of the input element when it changes
-  const color = themeInput.value;
-  document.documentElement.style.setProperty('--primary-theme-color', color);
-});
